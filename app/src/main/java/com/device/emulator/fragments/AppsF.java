c1f161735a0e3d9d660264b3006316667f36de2f
r1shhh1 <rishi29ganguly@gmail.com>
2023-03-27 00:34:39 +0530

    init

---
+++ app/src/main/java/com/device/emulator/fragments/AppsF.java
@@ -0,0 +1,31 @@
+package com.device.emulator.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.device.emulator.R;
+
+public class AppsF extends Fragment {
+    public AppsF() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_apps, container, false);
+
+    }
+}
